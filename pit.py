ee5f2990b682b4cda50e7451c42668ef7315ca22
Jon Mockbee <mockbee1000@gmail.com>
2021-07-22 20:32:12 -0400

    final answer

--- pit.py
+++ pit.py
@@ -4,8 +4,8 @@ if income  <= 85528:
     tax = ((income *.18) -556.02) 
 # Write your code here.
 
-else income > 85528:
-tax = ((income -14839.02) + .32*tax)
+else:
+    tax = ((income -14839.02) + .32* tax)
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
